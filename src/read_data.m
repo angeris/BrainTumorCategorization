f70dfdde4dc0710982c175b8705fef38d62dac0e
Guillermo Angeris <guillean@stanford.edu>
2015-11-10 16:56:23 -0800

    Sorting some more stuff

--- src/read_data.m
+++ src/read_data.m
@@ -1,7 +1,7 @@
 function [ output_args ] = read_data( input_args )
-%READ_DATA Summary of this function goes here
-%   Detailed explanation goes here
+%READ_DATA Reads the data in a specified format.
 
+data = readMHA('data/brats_2013_pat0001_1/VSD.Brain_3more.XX.O.OT.54517');
 
 end
 
