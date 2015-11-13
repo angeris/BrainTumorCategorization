dc559df0e2adf8b623fdd7f59f924821f39464f4
Guillermo Angeris <guillean@stanford.edu>
2015-11-12 19:37:44 -0800

    Generalized the fft to n dimensions

--- src/convnd.m
+++ src/convnd.m
@@ -1,5 +1,5 @@
-function [ c ] = conv3d( a, b )
-%CONV3D Summary of this function goes here
+function [ c ] = convnd( a, b, n )
+%CONV3D convolves two n-dimensional arrays using the FFT.
 
 a_max = max(size(a));
 b_max = max(size(b));
@@ -9,7 +9,7 @@ s = a_max+b_max-1;
 a_fft = a;
 b_fft = b;
 
-for i=1:3
+for i=1:n
     a_fft = fft(a_fft, s, i);
     b_fft = fft(b_fft, s, i);
 end
@@ -17,7 +17,7 @@ end
 conv_fft = a_fft.*b_fft;
 
 c = conv_fft;
-for i=1:3
+for i=1:n
     c = ifft(c, [], i);
 end
 
