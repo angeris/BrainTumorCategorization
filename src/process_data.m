d5f593969147da84d743498055316a6a008b59f7
Guillermo Angeris <guillean@stanford.edu>
2015-11-12 19:11:12 -0800

    Added some stuff for process_data

--- src/process_data.m
+++ src/process_data.m
@@ -2,14 +2,13 @@ function [ classification ] = process_data( modal_pixels, size_hor, size_ver )
 %PROCESS_DATA Inputs the modality of the pixels and outputs each pixel's classification.
 
 K = 7;
+modal_pixels_t = modal_pixels'; % Classification is column-major.
 
-modal_pixels_t = modal_pixels';
-
-[i, clusters] = kmeans(modal_pixels_t, K);
+[i, ~] = kmeans(modal_pixels_t, K);
 
 image = reshape(i, size_hor, size_ver);
-
 imshow(image==1);
 
+classification = i;
 end
 
