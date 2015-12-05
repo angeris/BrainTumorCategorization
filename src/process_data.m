0853f90958b5d903dc1bd9c3926efe2be6521bcc
teundeplanque <teun@stanford.edu>
2015-12-05 13:24:16 -0800

    Added clustering algorithms
    Added k-medoids clustering, hierarchical clustering, and gaussian mixture models

--- src/process_data.m
+++ src/process_data.m
@@ -5,6 +5,17 @@ K = 6;
 
 disp(size(modal_pixels))
 
+% k-means clustering
 [classification, ~] = kmeans(modal_pixels, K);
+
+% k-medoids clustering
+%[classification, ~] = kmedoids(modal_pixels, K);
+
+% hierarchical clustering
+%classification = cluster(linkage(modal_pixels, 'average', 'euclidean'), 'maxclust', K);
+
+% gaussian mixture models
+%classification = cluster(fitgmdist(modal_pixels, K), modal_pixels);
+
 end
 
